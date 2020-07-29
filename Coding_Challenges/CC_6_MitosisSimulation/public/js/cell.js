fd77ee7146ba7440094fe97bdacbb826fb698fcc
Marc PIerre <marcsllite@gmail.com>
2020-07-28 23:03:45 -0400

    updated mitosis simulation

--- Coding_Challenges/CC_6_MitosisSimulation/public/js/cell.js
+++ Coding_Challenges/CC_6_MitosisSimulation/public/js/cell.js
@@ -1,9 +1,13 @@
 class Cell {
-  constructor(pos = createVector(random(width), random(height)), r = 10, c = color(255, 255, 255, 150)) {
-    this.pos = (pos instanceof p5.Vector)? pos.copy() : createVector();
+  constructor(pos, r = 10, c = color(255, 255, 255, 150)) {
+    this.pos = (pos instanceof p5.Vector)? 
+                pos.copy() : 
+                createVector(random(r, width - r), random(r, height - r));
     this.r = r;
     this.c = (c instanceof p5.Color)? c : color(255, 255, 255, 150);
     this.vel = p5.Vector.random2D();
+    this.growing = this.r <= 3;
+    console.log("R = :" + this.r);
   }
 
   setVel(vel = p5.Vector.random2D()) {
@@ -11,22 +15,42 @@ class Cell {
   }
 
   move() {
-    this.setVel();
+    // this.setVel();
     this.pos.add(this.vel);
+    this.checkBounds();
+  }
+
+  grow() {
+    if(this.growing) {
+      this.r += 0.5;
+      this.growing = this.r < 40;
+    }
   }
 
   show() {
     noStroke();
     fill(this.c);
-    ellipseMode(CENTER);
+    ellipseMode(RADIUS);
     ellipse(this.pos.x, this.pos.y, this.r, this.r);
+  }
+
+  checkBounds() {
+    var onTopEdge = this.pos.y < 0 + this.r;
+    var onBottomEdge = this.pos.y > height - this.r;
+    var onLeftEdge = this.pos.x < 0 + this.r;
+    var onRightEdge = this.pos.x > height - this.r;
+
+    if(onTopEdge || onBottomEdge) {
+      this.vel.y *= -1;
+    } else if(onLeftEdge || onRightEdge) {
+      this.vel.x *= -1;
+    }
   }
 
   clicked(x, y) {
     // TODO: fix overlapping cells thinking theyve been clicked
     var d = dist(this.pos.x, this.pos.y, x, y);
     if(d <= this.r) {
-      console.log(`Cell at (${this.pos.x}, ${this.pos.y}) was clicked`);
       return true;
     } else {
       return false;
@@ -34,8 +58,9 @@ class Cell {
   }
 
   divide() {
-    var cellA = new Cell(this.pos, this.r / 2, this.c);
-    var cellB = new Cell(this.pos, this.r / 2, this.c);
+    var newColor = color(random(255), random(255), random(255), 150)
+    var cellA = new Cell(this.pos, this.r / 2, newColor);
+    var cellB = new Cell(this.pos, this.r / 2, newColor);
     return [ cellA, cellB ];
   }
 }
--- Coding_Challenges/CC_6_MitosisSimulation/public/js/sketch.js
+++ Coding_Challenges/CC_6_MitosisSimulation/public/js/sketch.js
@@ -1,23 +1,66 @@
 var cells = [];
+var startingClicks;
+var previousClicks;
+var currentClicks;
+
 
 function setup() {
-  createCanvas(700, 700);
-  cells.push(new Cell(undefined, 80, color(random(100, 255), random(100, 255), 0, 150)));
+  createCanvas(500, 500);
+  startingClicks = 10;
+  previousClicks = startingClicks;
+  currentClicks = startingClicks;
+  cells.push(new Cell(undefined, 240, color(random(100, 255), random(100, 255), 0, 150)));
 }
 
 function draw() {
-  background(51);
+  background(255);
+  showLives();
+
   cells.forEach(cell => {
     cell.move();
     cell.show();
+    cell.grow();
   });
+
+  if(currentClicks <= 0) {
+    reset()
+  }
+}
+
+function showLives() { 
+  var txt = "Lives: " + currentClicks;
+  textSize(20);
+  if(currentClicks === previousClicks) {
+    fill(51, 51, 51, 150);
+  } else if(currentClicks > previousClicks) {
+    fill(32,178,170);
+  } else if(currentClicks < previousClicks) {
+    fill(240,128,128);
+  }
+  text(txt, width - 90, height - 20);
+}
+
+function reset() {
+  cells = [];
+  startingClicks = 10;
+  currentClicks = startingClicks;
+  cells.push(new Cell(undefined, 240, color(random(100, 255), random(100, 255), 0, 150)));
 }
 
 function mousePressed() {
+  previousClicks = currentClicks;
+  var count = 0;
   for(var i = cells.length - 1; i >= 0; i--) {
     if(cells[i].clicked(mouseX, mouseY)) {
+      count++;
+      currentClicks++;
       cells = cells.concat(cells[i].divide());
       cells.splice(i, 1);
+      break;
     }
   };
+
+  if(count === 0) {
+    currentClicks--;
+  }
 }
